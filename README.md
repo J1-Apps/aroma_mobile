5f623039b7e4050a3157a9e6e7ed589b8fc4f660
Jaewon Yang <j.yang@draftkings.com>
2025-05-23 16:41:27 -0400

    aroma: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aroma Mobile
+
+To help chefs find and develop their personal cooking identities, and share them with their community.
---
+++ android/app/src/main/kotlin/j1apps/aroma_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package j1apps.aroma_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
